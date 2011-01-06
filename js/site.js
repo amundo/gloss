8986d04b0276071d6e2dfedcd405ab9476b48c8e
Patrick Hall <pat@amundo.com>
2011-01-06 03:25:14 -0800

    basic layout, some functionality

--- js/site.js
+++ js/site.js
@@ -1,5 +1,36 @@
-$(document).ready(function($){
+$(document).ready(function(){
 
+   function compare(){
+     var Acount = $('#A').val().split(/ +/).length;
+     var Bcount = $('#B').val().split(/ +/).length;
+     return Acount == Bcount;
+   }
+
+   function zip(a,b){
+     var zipped = [];
+     for(var i=0;i<a.length;i++){
+       zipped.push( [a[i], b[i]] ) ; 
+     } 
+     console.log(zipped);
+     return zipped;
+   }
+
+   function gloss(){
+     var Awords = $.trim($('#A').val()).split(/ +/);
+     var Bwords = $.trim($('#B').val()).split(/ +/);
+     var zipped = zip(Awords, Bwords);
+   }
   
+   $('#A, #B').keydown(function(ev){
+     $this = $(this);
+     if (ev.which ==  13){
+       if(compare()){
+         gloss();
+       };
+       $this.val($.trim($this.val()));
+       return false; 
+     } 
+   })
 
-})(jQuery)
+
+})
