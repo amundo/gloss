a06098e3032b1674e8ed64bbd26b22fa58506f64
Patrick Hall <pat@amundo.com>
2011-01-06 19:04:22 -0800

    basically works

--- js/site.js
+++ js/site.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
    function compare(){
      var Acount = $('#A').val().split(/ +/).length;
+     $('#Acount').html(Acount);
      var Bcount = $('#B').val().split(/ +/).length;
+     $('#Bcount').html(Bcount);
+     if(Acount == Bcount) 
+       $('#status span').addClass('sameLength');
      return Acount == Bcount;
    }
 
@@ -11,26 +15,57 @@ $(document).ready(function(){
      for(var i=0;i<a.length;i++){
        zipped.push( [a[i], b[i]] ) ; 
      } 
-     console.log(zipped);
      return zipped;
    }
 
-   function gloss(){
+   function makePairs(){
      var Awords = $.trim($('#A').val()).split(/ +/);
      var Bwords = $.trim($('#B').val()).split(/ +/);
-     var zipped = zip(Awords, Bwords);
+     return zip(Awords, Bwords);
    }
-  
+
+   function assembleGlosses(pairs){
+    
+     var glosses = [];
+     for(var i=0;i<pairs.length;i++){
+       var word = pairs[i][0];
+       var morph = pairs[i][1];
+
+       glosses.push(
+         {'word': word, 'morph': morph}
+       );
+
+     }
+     return glosses;
+   }
+
+   compare();
+
+   function renderGloss(gloss){
+     $("#gloss").html($( "#glossTemplate" ).tmpl( gloss ));
+   }
+
    $('#A, #B').keydown(function(ev){
      $this = $(this);
      if (ev.which ==  13){
        if(compare()){
-         gloss();
-       };
-       $this.val($.trim($this.val()));
-       return false; 
+
+         var pairs = makePairs();
+         var glosses = assembleGlosses(pairs);
+
+         gloss = {
+           'glosses' : glosses,
+           'sentence' : $('#A').val(),
+           'translation' : $('#B').val()
+         };
+
+         renderGloss(gloss);
+ 
+         $this.val($.trim($this.val()));
+
+         return false; 
+       } 
      } 
    })
-
 
 })
