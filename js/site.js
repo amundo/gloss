a67cefa990b4ea3a870aabee37f966b233d681ab
Patrick Hall <pat@amundo.com>
2011-01-07 21:22:31 -0800

    simplify layout. having some issues with whitespace

--- js/site.js
+++ js/site.js
@@ -2,47 +2,55 @@ $(document).ready(function(){
 
   $('#box').autogrow();
 
-  var chunks = [],
-      gloss = {},
-      text = $('#box').val(),
-      lines = $.trim(text).split(/\n\n+/),
-      sentence = lines.shift(),
-      translation = lines.pop(),
-      stanza = lines.join('\n\n');
+  function renderGloss(){
+    var chunks = [],
+        gloss = {},
+        text = $('#box').val(),
+        lines = $.trim(text).split(/\n\n+/),
+        sentence = lines.shift(),
+        translation = lines.pop(),
+        stanza = lines.join('\n\n');
 
-  function parseGlosses(stanza){
-    var pairs = stanza.split(/\n\n+/),
-        glosses = [];
+    function parseGlosses(stanza){
+      var pairs = stanza.split(/\n\n+/),
+          glosses = [];
 
-    $.each(pairs, function(i, pair){
-      var parts = $.trim(pair).split(/\n/),
-          word = $.trim(parts[0]),
-          morph = $.trim(parts[1]);
-     
-      glosses.push(
-        {'word': word, 'morph': morph}
-      );
-    }) 
+      $.each(pairs, function(i, pair){
+        var parts = $.trim(pair).split(/\n/),
+            word = $.trim(parts[0]),
+            morph = $.trim(parts[1]);
+       
+        glosses.push(
+          {'word': word, 'morph': morph}
+        );
+      }) 
 
-    return glosses;
+      return glosses;
+    }
+
+    gloss = {
+      'sentence' : sentence,
+      'translation' : translation,
+      'glosses' : parseGlosses(stanza)
+    }
+       
+/*
+    if (console.log) { 
+      console.log('sentence: ' + sentence);
+      console.log('translation: ' + translation);
+      console.log(gloss);
+    }
+*/
+
+    return gloss;
   }
 
-  gloss = {
-    'sentence' : sentence,
-    'translation' : translation,
-    'glosses' : parseGlosses(stanza)
-  }
-     
-  if (console.log) { 
-    console.log('sentence: ' + sentence);
-    console.log('translation: ' + translation);
-    console.log(gloss);
-  }
 
-
-  $('#box').keydown(function(){
-
-
+  $('#box').keydown(function(event){
+    if ( event.which == 13 && event.shiftKey ) {
+      gloss = renderGloss();
+      $("#gloss").html($( "#glossTemplate" ).tmpl( gloss ));
+    }
   })
 
 })
